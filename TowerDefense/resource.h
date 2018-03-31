155278f74a0d3be46c80773c3659d30a5b4c5b75
Isaiah Hoffman <iyhoff13@gmail.com>
2018-03-31 12:02:26 -0400

    Updated resource files.
    Signed-off-by: Isaiah Hoffman <iyhoff13@gmail.com>

--- TowerDefense/resource.h
+++ TowerDefense/resource.h
@@ -2,13 +2,41 @@
 // Microsoft Visual C++ generated include file.
 // Used by resource.rc
 //
+#define IDR_MAIN_MENU                   101
+#define IDR_TERRAIN_EDITOR_MENU         102
+#define ID_ACTIONS                      40009
+#define ID_MM_FILE_NEW_GAME             40101
+#define ID_MM_FILE_LOAD_GAME            40102
+#define ID_MM_FILE_SAVE_GAME            40103
+#define ID_MM_FILE_SAVE_GAME_AS         40104
+#define ID_MM_FILE_QUIT                 40105
+#define ID_MM_ACTIONS_TOGGLE_PAUSE      40201
+#define ID_MM_ACTIONS_NEXT_WAVE         40202
+#define ID_MM_TOWERS_NONE               40301
+#define ID_MM_DEVELOP_TERRAIN_EDITOR    40501
+#define ID_TE_FILE_NEW_MAP              41101
+#define ID_TE_FILE_OPEN_MAP             41102
+#define ID_TE_FILE_SAVE_MAP             41103
+#define ID_TE_FILE_SAVE_MAP_AS          41104
+#define ID_TE_FILE_QUIT                 41105
+#define ID_TE_ACTIONS_SET_GROUND_START  41201
+#define ID_TE_ACTIONS_SET_AIR_START     41202
+#define ID_TE_ACTIONS_SET_GROUND_END    41203
+#define ID_TE_ACTIONS_SET_AIR_END       41204
+#define ID_TE_TERRAIN_TYPES_NONE        41301
+#define ID_TE_TERRAIN_TYPES_GRASS       41302
+#define ID_TE_TERRAIN_TYPES_SWAMP       41303
+#define ID_TE_TERRAIN_TYPES_FOREST      41304
+#define ID_TE_TERRAIN_TYPES_CAVE        41305
+#define ID_TE_TERRAIN_TYPES_OCEAN       41306
+#define ID_TE_TERRAIN_TYPES_MOUNTAIN    41307
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
-#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40010
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
