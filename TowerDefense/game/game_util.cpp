c9c3811986737d8fd012a2d23057cb493250908e
Isaiah Hoffman <iyhoff13@gmail.com>
2018-04-16 16:19:57 -0400

    Random number generation stuff...
    Signed-off-by: Isaiah Hoffman <iyhoff13@gmail.com>

---
+++ TowerDefense/game/game_util.cpp
@@ -0,0 +1,16 @@
+// File Author: Isaiah Hoffman
+// File Created: April 14, 2018
+#include <random>
+#include "./game_util.hpp"
+namespace hoffman::isaiah {
+	namespace game {
+		namespace rng {
+			thread_local std::random_device rd {};
+#ifdef _WIN64
+			thread_local std::mt19937_64 gen {rd()};
+#else
+			thread_local std::mt19937 gen {rd()};
+#endif
+		}
+	}
+}
---
+++ TowerDefense/game/game_util.hpp
@@ -0,0 +1,19 @@
+#pragma once
+// File Author: Isaiah Hoffman
+// File Created: April 14, 2018
+#include <random>
+
+namespace hoffman::isaiah {
+	namespace game {
+		namespace rng {
+			/// <summary>A non-deterministic random number generator used to seed another random number generator.</summary>
+			extern thread_local std::random_device rd;
+			/// <summary>Deterministic random number generator.</summary>
+#ifdef _WIN64
+			extern thread_local std::mt19937_64 gen;
+#else
+			extern thread_local std::mt19937 gen;
+#endif
+		}
+	}
+}
