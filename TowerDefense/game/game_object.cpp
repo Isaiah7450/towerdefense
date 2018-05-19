ccad39616c1a000b9f4de584ff50700f5d599bc4
Isaiah Hoffman <iyhoff13@gmail.com>
2018-05-19 11:27:04 -0400

    Base class for game objects.
    Signed-off-by: Isaiah Hoffman <iyhoff13@gmail.com>

---
+++ TowerDefense/game/game_object.cpp
@@ -0,0 +1,75 @@
+// File Author: Isaiah Hoffman
+// File Created: May 15, 2018
+#include <memory>
+#include <array>
+#include <cmath>
+#include "./../globals.hpp"
+#include "./../ih_math.hpp"
+#include "./../graphics/graphics.hpp"
+#include "./../graphics/shapes.hpp"
+#include "./game_object.hpp"
+
+namespace hoffman::isaiah {
+	namespace game {
+		GameObject::GameObject(std::shared_ptr<graphics::DX::DeviceResources2D> dev_res, graphics::shapes::ShapeTypes st,
+			graphics::Color o_color, graphics::Color f_color, double cgx, double cgy, double gw, double gh) :
+			sprite {nullptr},
+			gx {cgx},
+			gy {cgy} {
+			const float csx = static_cast<float>(graphics::convertToScreenX(cgx));
+			const float csy = static_cast<float>(graphics::convertToScreenY(cgy));
+			const float sw = static_cast<float>(gw) * graphics::getGameSquareWidth<float>();
+			const float sh = static_cast<float>(gh) * graphics::getGameSquareHeight<float>();
+
+			switch (st) {
+			case graphics::shapes::ShapeTypes::Star:
+			{
+				const std::array<std::array<float, 2>, 10> points {{
+					{csx + sw / 2.f, csy},
+					{csx + sw / 4.f * std::cos(0.2f * math::calculate_pi<float>()),
+					csy + sh / 4.f * std::sin(0.2f * math::calculate_pi<float>())},
+					{csx + sw / 2.f * std::cos(0.4f * math::calculate_pi<float>()),
+					csy + sh / 2.f * std::sin(0.4f * math::calculate_pi<float>())},
+					{csx + sw / 4.f * std::cos(0.6f * math::calculate_pi<float>()),
+					csy + sh / 4.f * std::sin(0.6f * math::calculate_pi<float>())},
+					{csx + sw / 2.f * std::cos(0.8f * math::calculate_pi<float>()),
+					csy + sh / 2.f * std::sin(0.8f * math::calculate_pi<float>())},
+					{csx + sw / 4.f * std::cos(1.0f * math::calculate_pi<float>()),
+					csy + sh / 4.f * std::sin(1.0f * math::calculate_pi<float>())},
+					{csx + sw / 2.f * std::cos(1.2f * math::calculate_pi<float>()),
+					csy + sh / 2.f * std::sin(1.2f * math::calculate_pi<float>())},
+					{csx + sw / 4.f * std::cos(1.4f * math::calculate_pi<float>()),
+					csy + sh / 4.f * std::sin(1.4f * math::calculate_pi<float>())},
+					{csx + sw / 2.f * std::cos(1.6f * math::calculate_pi<float>()),
+					csy + sh / 2.f * std::sin(1.6f * math::calculate_pi<float>())},
+					{csx + sw / 4.f * std::cos(1.8f * math::calculate_pi<float>()),
+					csy + sh / 4.f * std::sin(1.8f * math::calculate_pi<float>())}
+				}};
+				this->sprite = std::make_unique<graphics::shapes::Shape2DPolygon<10>>(dev_res, o_color, f_color,
+					points, csx, csy);
+				this->rotate(math::calculate_pi<float>() * 1.5f);
+				break;
+			}
+			case graphics::shapes::ShapeTypes::Diamond:
+				this->sprite = std::make_unique<graphics::shapes::Shape2DDiamond>(dev_res, o_color, f_color,
+					csx, csy, sw, sh);
+				break;
+			case graphics::shapes::ShapeTypes::Rectangle:
+				this->sprite = std::make_unique<graphics::shapes::Shape2DRectangle>(dev_res, o_color, f_color,
+					csx - sw / 2.f, csy - sh / 2.f, csx + sw / 2.f, csy + sh / 2.f);
+				break;
+			case graphics::shapes::ShapeTypes::Triangle:
+			{
+				this->sprite = std::make_unique<graphics::shapes::Shape2DTriangle>(dev_res, o_color, f_color,
+					csx - sw / 2.f, csy - sh / 2.f, csx, csy + sh / 2.f, csx + sw / 2.f, csy - sh / 2.f);
+				break;
+			}
+			case graphics::shapes::ShapeTypes::Ellipse:
+			default:
+				this->sprite = std::make_unique<graphics::shapes::Shape2DEllipse>(dev_res, o_color, f_color,
+					csx, csy, sw, sh);
+				break;
+			}
+		}
+	}
+}
---
+++ TowerDefense/game/game_object.hpp
@@ -0,0 +1,74 @@
+#pragma once
+// File Author: Isaiah Hoffman
+// File Created: May 15, 2018
+#include <memory>
+#include "./../globals.hpp"
+#include "./../graphics/graphics.hpp"
+#include "./../graphics/shapes.hpp"
+
+namespace hoffman::isaiah {
+	namespace game {
+		/// <summary>Base class for game objects that are drawn on the screen.</summary>
+		class GameObject : public graphics::IDrawable {
+		public:
+			GameObject(std::shared_ptr<graphics::DX::DeviceResources2D> dev_res, graphics::shapes::ShapeTypes st,
+				graphics::Color o_color, graphics::Color f_color, double cgx, double cgy, double gw, double gh);
+			// Implements graphics::Drawable::draw()
+			void draw(const graphics::Renderer2D& renderer) const noexcept {
+				this->sprite->draw(renderer);
+			}
+
+			bool checkHit(float sx, float sy) const noexcept {
+				return this->sprite->checkHit(sx, sy);
+			}
+			bool intersects(const GameObject& other) const noexcept {
+				return this->sprite->intersect(other.sprite.get());
+			}
+
+			// Setters and Changers
+			void translate(double dgx, double dgy) {
+				this->changeGameX(dgx);
+				this->changeGameY(dgy);
+				this->sprite->change_translate(static_cast<float>(graphics::convertToScreenX(dgx)), 0.f);
+				this->sprite->change_translate(0.f, static_cast<float>(graphics::convertToScreenY(dgy)));
+			}
+			void rotate(float new_theta) {
+				this->sprite->change_rotation(new_theta);
+			}
+
+			// Getters
+			/// <returns>Returns the game x-coordinate of the object.</returns>
+			double getGameX() const noexcept {
+				return this->gx;
+			}
+			/// <returns>Returns the game y-coordinate of the object.</returns>
+			double getGameY() const noexcept {
+				return this->gy;
+			}
+			/// <returns>Returns the screen x-coordinate of the object.</returns>
+			double getScreenX() const noexcept {
+				return graphics::convertToScreenX(this->getGameX());
+			}
+			/// <returns>Returns the screen y-coordinate of the object.</returns>
+			double getScreenY() const noexcept {
+				return graphics::convertToScreenY(this->getGameY());
+			}
+		protected:
+			// Setters and Changers
+			// Note that these don't perform actual changes to the graphical representation.
+			void changeGameX(double dgx) {
+				this->gx += dgx;
+			}
+			void changeGameY(double dgy) {
+				this->gy += dgy;
+			}
+		private:
+			/// <summary>The graphical representation of the object.</summary>
+			std::unique_ptr<graphics::shapes::Shape2DBase> sprite;
+			/// <summary>The object's game x-coordinate.</summary>
+			double gx;
+			/// <summary>The object's game y-coordinate.</summary>
+			double gy;
+		};
+	}
+}
