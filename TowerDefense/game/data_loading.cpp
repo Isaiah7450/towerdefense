865f8e08039d85a7c0373363075117f7774c2805
Isaiah Hoffman <iyhoff13@gmail.com>
2018-05-24 19:04:11 -0400

    Enemy buffs can now be read.
    Signed-off-by: Isaiah Hoffman <iyhoff13@gmail.com>

--- TowerDefense/game/data_loading.cpp
+++ TowerDefense/game/data_loading.cpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <map>
 #include <vector>
+#include <stdexcept>
 #include "./../file_util.hpp"
 #include "./../globals.hpp"
 #include "./../graphics/graphics.hpp"
@@ -38,9 +39,9 @@ namespace hoffman::isaiah {
 			if (!util::file::matchToken(util::file::TokenTypes::Section, L"buff_targets"s, my_token)) {
 				throw util::file::DataFileException {L"Expected the [buff_targets] section."s, line};
 			}
+			// Key => Group Name, Value => List of enemy names associated with that group name
+			std::map<std::wstring, std::vector<std::wstring>> buff_target_groups {};
 			if (util::file::verifyObjectStart(L"targets"s, data_file, line)) {
-				// Key => Group Name, Value => List of enemy names associated with that group name
-				std::map<std::wstring, std::vector<std::wstring>> buff_target_groups {};
 				while (true) {
 					my_token = util::file::getNextToken(data_file, line);
 					if (util::file::matchToken(util::file::TokenTypes::Object, L"}"s, my_token)
@@ -137,7 +138,110 @@ namespace hoffman::isaiah {
 				my_token = util::file::getKeyValue(L"is_flying"s, data_file, line);
 				bool fly = util::file::parseBoolean(my_token, line);
 				if (util::file::verifyObjectStart(L"buffs", data_file, line)) {
-					// TODO: Buffs
+					std::vector<std::shared_ptr<BuffBase>> my_buffs {};
+					my_token = util::file::getNextToken(data_file, line);
+					do {
+						if (!util::file::matchToken(util::file::TokenTypes::Object, L"{"s, my_token)) {
+							throw util::file::DataFileException {L"Expected opening brace ({)."s, line};
+						}
+						my_token = util::file::getKeyValue(L"type"s, data_file, line);
+						BuffTypes buff_type = my_token.second == L"Intelligence"s ? BuffTypes::Intelligence
+							: my_token.second == L"Speed"s ? BuffTypes::Speed
+							: my_token.second == L"Healer"s ? BuffTypes::Healer
+							: my_token.second == L"Purify"s ? BuffTypes::Purify
+							: throw util::file::DataFileException {L"Expected the type of the buff."s
+								L" Valid values include: Intelligence, Speed, Healer, Purify"s, line};
+						my_token = util::file::getKeyValue(L"targets"s, data_file, line);
+						std::wstring buff_group = util::file::parseString(my_token, line);
+						my_token = util::file::getKeyValue(L"radius"s, data_file, line);
+						try {
+							// Gotta find some way to tell the compiler NOT
+							// to optimize away this statement.
+							(void)buff_target_groups.at(buff_group);
+						}
+						catch (const std::out_of_range&) {
+							throw util::file::DataFileException {L"Invalid target group name specified."s, line};
+						}
+						double buff_radius = util::file::parseNumber(my_token, line);
+						if (buff_radius <= 0.0) {
+							throw util::file::DataFileException {L"Buff radius must be positive."s, line};
+						}
+						my_token = util::file::getKeyValue(L"delay"s, data_file, line);
+						int buff_delay = static_cast<int>(util::file::parseNumber(my_token, line));
+						if (buff_delay < 10 || buff_delay > 60000) {
+							throw util::file::DataFileException {L"Buff delay must be between 10 and 60,000 inclusive."s,
+								line};
+						}
+						[[maybe_unused]] int buff_duration {};
+						// Duration is common to many buffs, so I don't switch on it.
+						if (buff_type == BuffTypes::Intelligence || buff_type == BuffTypes::Speed) {
+							my_token = util::file::getKeyValue(L"duration"s, data_file, line);
+							buff_duration = static_cast<int>(util::file::parseNumber(my_token, line));
+						}
+						// Read buff-specific attributes
+						switch (buff_type) {
+						case BuffTypes::Intelligence:
+						{
+							auto smart_buff = std::make_shared<SmartBuff>(buff_target_groups.at(buff_group),
+								buff_radius, buff_delay, buff_duration);
+							my_buffs.emplace_back(std::move(smart_buff));
+							break;
+						}
+						case BuffTypes::Speed:
+						{
+							my_token = util::file::getKeyValue(L"walking_speed_boost"s, data_file, line);
+							double buff_wspd = util::file::parseNumber(my_token, line);
+							if (buff_wspd < 0.0) {
+								throw util::file::DataFileException {L"Walking speed boost should be non-negative."s, line};
+							}
+							my_token = util::file::getKeyValue(L"running_speed_boost"s, data_file, line);
+							double buff_rspd = util::file::parseNumber(my_token, line);
+							if (buff_rspd < 0.0) {
+								throw util::file::DataFileException {L"Running speed boost should be non-negative."s, line};
+							}
+							my_token = util::file::getKeyValue(L"injured_speed_boost"s, data_file, line);
+							double buff_ispd = util::file::parseNumber(my_token, line);
+							if (buff_ispd < 0.0) {
+								throw util::file::DataFileException {L"Injured speed boost should be non-negative."s, line};
+							}
+							if (buff_ispd == 0.0 && buff_rspd == 0. && buff_wspd == 0.) {
+								throw util::file::DataFileException {L"You should set at least one of the speed"s
+									L" boosts to a positive value!"s, line};
+							}
+							auto speed_buff = std::make_shared<SpeedBuff>(buff_target_groups.at(buff_group),
+								buff_radius, buff_delay, buff_duration, buff_wspd, buff_rspd, buff_ispd);
+							my_buffs.emplace_back(std::move(speed_buff));
+							break;
+						}
+						case BuffTypes::Healer:
+						{
+							my_token = util::file::getKeyValue(L"heal_amount"s, data_file, line);
+							double buff_heal = util::file::parseNumber(my_token, line);
+							if (buff_heal <= 0.0) {
+								throw util::file::DataFileException {L"Heal amount should be positive."s, line};
+							}
+							auto heal_buff = std::make_shared<HealerBuff>(buff_target_groups.at(buff_group),
+								buff_radius, buff_delay, buff_heal);
+							my_buffs.emplace_back(std::move(heal_buff));
+							break;
+						}
+						case BuffTypes::Purify:
+						{
+							my_token = util::file::getKeyValue(L"purify_max_effects"s, data_file, line);
+							int buff_cure_max = static_cast<int>(util::file::parseNumber(my_token, line));
+							auto purify_buff = std::make_shared<PurifyBuff>(buff_target_groups.at(buff_group),
+								buff_radius, buff_delay, buff_cure_max);
+							break;
+						}
+						} // End switch, this looks weird but is correct
+						my_token = util::file::getNextToken(data_file, line);
+						if (!util::file::matchToken(util::file::TokenTypes::Object, L"}"s, my_token)) {
+							throw util::file::DataFileException {L"Expected closing brace (})."s, line};
+						}
+						my_token = util::file::getNextToken(data_file, line);
+					} while (!util::file::matchToken(util::file::TokenTypes::Object, L"}"s, my_token));
+					auto my_type = std::make_unique<EnemyType>(n, d, c, st, dmg, hp, ahp, ar, pt,
+						wspd, rspd, ispd, strat, move_diag, fly, std::move(my_buffs));
 				}
 				else {
 					auto my_type = std::make_unique<EnemyType>(n, d, c, st, dmg, hp, ahp, ar, pt,
