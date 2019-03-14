f8c78e3bad5cd6bbd4f67696ef7d395e51c12d78
Isaiah Hoffman <iyhoff13@gmail.com>
2019-03-14 13:01:15 -0400

    Added macro that will hopefully improve build times slightly.
    Signed-off-by: Isaiah Hoffman <iyhoff13@gmail.com>

--- TowerDefense/targetver.hpp
+++ TowerDefense/targetver.hpp
@@ -4,3 +4,5 @@
 #define WINVER 0x0601
 #define _WIN32_WINNT WINVER
 #include <SDKDDKVer.h>
+// (Build times are important, and the less code read, the better in that respect.)
+#define WIN32_LEAN_AND_MEAN
