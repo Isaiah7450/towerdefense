026931f2358791113a1f54a0148a066faf46bf78
Isaiah Hoffman <iyhoff13@gmail.com>
2019-03-14 19:46:51 -0400

    Some changes to the pathfinding algorithm to improve performance.
    Signed-off-by: Isaiah Hoffman <iyhoff13@gmail.com>

--- TowerDefense/pathfinding/pathfinder.cpp
+++ TowerDefense/pathfinding/pathfinder.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <map>
 #include <utility>
+#include <algorithm>
 #include "./../globals.hpp"
 #include "./graph_node.hpp"
 #include "./grid.hpp"
@@ -68,13 +69,13 @@ namespace hoffman::isaiah {
 				? this->terrain_graph.getNode(goal_x, goal_y)
 				: *this->terrain_graph.getGoalNode();
 			std::priority_queue<PathFinderNode, std::vector<PathFinderNode>, PathFinderComparator> my_set {};
-			std::map<int, double> previous_costs {};
+			std::vector<std::pair<int, double>> previous_costs {};
 			// Yes, I am actually going to do the search in reverse order so
 			// that the beginning of the queue has the start node.
 			auto my_goal = PathFinderNode {goal_node, nullptr, start_node,
 				this->heuristic_strategy, 0, j_multiplier, h_modifier};
 			my_set.push(my_goal);
-			previous_costs.emplace(determine_node_index(my_goal.getGameX(), my_goal.getGameY()), my_goal.getF());
+			previous_costs.emplace_back(determine_node_index(my_goal.getGameX(), my_goal.getGameY()), my_goal.getF());
 			while (!my_set.empty()) {
 				auto current_node = std::make_shared<PathFinderNode>(my_set.top());
 				if (current_node->getGameX() == start_node.getGameX() &&
@@ -90,15 +91,52 @@ namespace hoffman::isaiah {
 						start_node, this->heuristic_strategy,
 						static_cast<double>(influence_graph.getNode(neighbor_node->getGameX(),
 							neighbor_node->getGameY()).getWeight()), j_multiplier, h_modifier};
-					auto next_node_index = determine_node_index(next_node.getGameX(), next_node.getGameY());
+					const auto next_node_index = determine_node_index(next_node.getGameX(), next_node.getGameY());
 					// Check F score and only add if F score is lower
-					if (previous_costs.find(next_node_index) == previous_costs.end()) {
-						previous_costs.emplace(next_node_index, next_node.getF());
-						my_set.push(next_node);
+					struct MyCompare {
+						bool operator()(const std::pair<int, double>& a, const int& b) {
+							return a.first < b;
+						}
+						bool operator()(const int& a, const std::pair<int, double>& b) {
+							return a < b.first;
+						}
+					};
+					// Binary Search
+					size_t low_bound = 0;
+					size_t high_bound = previous_costs.size();
+					size_t my_index = high_bound / 2;
+					bool found = false;
+					while (true) {
+						const auto node_index = previous_costs.at(my_index).first;
+						if (node_index == next_node_index) {
+							// Found it!
+							if (previous_costs.at(my_index).second > next_node.getF()) {
+								previous_costs.at(my_index).second = next_node.getF();
+								my_set.push(next_node);
+							}
+							found = true;
+							break;
+						}
+						else if (low_bound >= high_bound) {
+							// Does not exist!
+							break;
+						}
+						else if (node_index > next_node_index) {
+							// Too high!
+							high_bound = my_index > 0 ? my_index - 1 : 0;
+							my_index = (my_index + low_bound) / 2;
+						}
+						else {
+							// Too low!
+							low_bound = my_index + 1;
+							my_index = (my_index + high_bound) / 2;
+						}
 					}
-					else if (previous_costs.at(next_node_index) > next_node.getF()) {
-						previous_costs.erase(next_node_index);
-						previous_costs.emplace(next_node_index, next_node.getF());
+					if (!found) {
+						const auto insert_loc = std::find_if(previous_costs.begin(), previous_costs.end(), [next_node_index](const auto& elem) {
+							return elem.first > next_node_index;
+						});
+						previous_costs.insert(insert_loc, std::make_pair<>(next_node_index, next_node.getF()));
 						my_set.push(next_node);
 					}
 				}
