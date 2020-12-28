bf9c9487d7f946c7b492d0245581ce605765d46d
Isaiah Hoffman <iyhoff13@gmail.com>
2020-12-28 16:19:10 -0500

    Using VS2019 to compile now.
    Signed-off-by: Isaiah Hoffman <iyhoff13@gmail.com>

---
+++ MyTests/targetver.hpp
@@ -0,0 +1,8 @@
+#pragma once
+// File Author: Isaiah Hoffman
+// File Created: March 9, 2018
+#define WINVER 0x0601
+#define _WIN32_WINNT WINVER
+#include <SDKDDKVer.h>
+// (Build times are important, and the less code read, the better in that respect.)
+#define WIN32_LEAN_AND_MEAN
